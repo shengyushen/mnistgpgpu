ff3be0b60bd9fca47ec003588d1933b0dc34c6fd
Ubuntu <ubuntu@ip-172-31-19-24.us-east-2.compute.internal>
2019-03-26 01:38:56 +0000

    adding new scripts for plt

---
+++ plt.plt
@@ -0,0 +1,7 @@
+set title "gpu ipc"      ;
+plot	"awkres_bs1_l100"  u 5:4 w linesp,\
+			"awkres_bs1_l200"  u 5:4 w linesp,\
+			"awkres_bs1_l400"  u 5:4 w linesp,\
+			"awkres_bs1_l800"  u 5:4 w linesp,\
+			"awkres_bs16_l100" u 5:4 w linesp,\
+			"awkres_bs16_l200" u 5:4 w linesp
--- plt.sh
+++ plt.sh
@@ -1,8 +1,14 @@
-grep -v "^GPGPU-Sim" xx10_l100 |awk -f plt.awk  > awkres_l100
-grep -v "^GPGPU-Sim" xx10_l200 |awk -f plt.awk  > awkres_l200
-grep -v "^GPGPU-Sim" xx10_l400 |awk -f plt.awk  > awkres_l400
-grep -v "^GPGPU-Sim" xx10_l800 |awk -f plt.awk  > awkres_l800
+
+for bs in 1 16 64 256
+do 
+	for l in 100 200 400 800
+	do
+		grep -v "^GPGPU-Sim" xx10_bs${bs}_l${l} |awk -f plt.awk  > awkres_bs${bs}_l${l}
+	done
+done 
 #gnuplot -p -e 'set title "gpu\_sim\_cycle";plot "awkres_l100" u 5:2 w linesp,"awkres_l200" u 5:2 w linesp,"awkres_l400" u 5:2 w linesp,"awkres_l800" u 5:2 w linesp'
 #gnuplot -p -e 'set title "gpu\_sim\_insn" ;plot "awkres_l100" u 5:3 w linesp,"awkres_l200" u 5:3 w linesp,"awkres_l400" u 5:3 w linesp,"awkres_l800" u 5:3 w linesp'
-gnuplot -p -e 'set title "gpu\_ipc"      ;plot "awkres_l100" u 5:4 w linesp,"awkres_l200" u 5:4 w linesp,"awkres_l400" u 5:4 w linesp,"awkres_l800" u 5:4 w linesp'
+#gnuplot -p -e 'set title "gpu ipc"      ;plot "awkres_bs1_l100" u 5:4 w linesp,"awkres_bs1_l200" u 5:4 w linesp,"awkres_bs1_l400" u 5:4 w linesp,"awkres_bs1_l800" u 5:4 w linesp,"awkres_bs16_l100" u 5:4 w linesp,"awkres_bs16_l200" u 5:4 w linesp,"awkres_bs16_l400" u 5:4 w linesp,"awkres_bs16_l800" u 5:4 w linesp'
 #gnuplot -p -e 'set title "gpu\_occupancy";plot "awkres_l100" u 5:9 w linesp,"awkres_l200" u 5:9 w linesp,"awkres_l400" u 5:9 w linesp,"awkres_l800" u 5:9 w linesp'
+gnuplot ./plt.plt
+
